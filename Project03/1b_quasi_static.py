5d49bc101a892ffc652b2b6290ee7f156bb4cef3
Omkar <omkarakmo@gmail.com>
2019-03-03 23:20:08 -0600

    Commit

--- Project03/1b_quasi_static.py
+++ Project03/1b_quasi_static.py
@@ -24,8 +24,8 @@ length = 1;
 W = 0.2;
 H = 0.2;
 
-a = 0.04;
-b = 0.4;
+a = 0.04/length;
+b = 0.4*H/length;
 area = a*b;
 
 F = -100
@@ -88,7 +88,7 @@ u_init = TrialFunction(V)
 d = u_init.geometric_dimension()
 v = TestFunction(V)
 f = Constant((0.0,0.0,0.0))
-T_init = Expression(('0.0', 'x[0] >= 0.48*l && x[0] <= 52 && near(x[1],w) && x[2] >= 0.3*h && x[2] <= 0.7*h? A : 0.0' ,'0.0'), degree=1, l=l_nd, w=w_nd,h=h_nd, A=traction_nd)
+T_init = Expression(('0.0', 'x[0] >= 0.48*l && x[0] <= .52*l && near(x[1],w) && x[2] >= 0.3*h && x[2] <= 0.7*h? A : 0.0' ,'0.0'), degree=1, l=l_nd, w=w_nd,h=h_nd, A=traction_nd)
 F_init = inner(sigma(u_init),epsilon(v))*dx - dot(f,v)*dx - dot(T_init,v)*ds
 a_init, L_init = lhs(F_init), rhs(F_init)
 
@@ -101,56 +101,3 @@ print(w[1])
 
 vtkfile_u = File('deflection.pvd')
 vtkfile_u << u_init
-
-## END ##
-
-# mesh = BoxMesh(Point(0,0,0),Point(length,W,H),10,3,3)
-# V = VectorFunctionSpace(mesh,'P',1)
-
-# tol = 1E-14
-
-# def boundary_left(x,on_boundary):
-# 	return (on_boundary and near(x[0],0))
-
-# def boundary_right(x,on_boundary):
-# 	return on_boundary and near(x[0],length)
-
-# bc_left = DirichletBC(V,Constant((0,0,0)),boundary_left)
-# bc_right = DirichletBC(V,Constant((0,0,0)),boundary_right)
-
-# def epsilon(u):
-# 	return 0.5*(nabla_grad(u) + nabla_grad(u).T)
-
-# def sigma(u):
-# 	return lambda_*nabla_div(u)*Identity(d) + mu*(epsilon(u) + epsilon(u).T)
-
-# u = TrialFunction(V)
-# d = u.geometric_dimension()
-# v = TestFunction(V)
-
-# f = Constant((0,0,0))
-# T = Expression(('0.0','(x[0] >= a && x[0] <= (L-a) && near(x[1],W)) ? (F/(H*(L-2*a))) : 0.0','0.0'),L=length,a=A,F=F,W=W,H=H,degree=1)
-
-# a = inner(sigma(u),epsilon(v))*dx
-# L = dot(f,v)*dx + dot (T,v)*ds
-
-# u = Function(V)
-# solve(a == L, u, [bc_left,bc_right])
-
-# w = u(length/2.0,W/2.0,H/2.0)
-# print(w[1])
-
-# vtkfile_u = File('deflection.pvd')
-# vtkfile_u << u
-
-# W = TensorFunctionSpace(mesh, "Lagrange", 1)
-# stress =  lambda_*nabla_div(u)*Identity(d) + mu*(epsilon(u) + epsilon(u).T)
-
-# vtkfile_s = File('stress.pvd')
-# vtkfile_s << project(stress,W)
-
-# s = sigma(u) - (1./3)*tr(sigma(u))*Identity(d) # deviatoric stress
-# von_Mises = sqrt(3./2*inner(s, s))
-# X = FunctionSpace(mesh, 'P', 1)
-# vtkfile_von = File('von_Mises.pvd')
-# vtkfile_von << project(von_Mises, X)
